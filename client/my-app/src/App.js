368594293d8d8c331c8483ac84e2d2d05bf0eb7d
Gaurang105 <gujratigaurang@gmail.com>
2023-06-12 03:18:44 +0530

    client update

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,31 +1,36 @@
 // App.js
 import React, { useState, useEffect } from 'react';
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://flask-socket.onrender.com/";
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'https://flask-socket.onrender.com/';
 
 function App() {
-  const [response, setResponse] = useState("");
-  const [input, setInput] = useState("");
+  const [response, setResponse] = useState('');
+  const [input, setInput] = useState('');
+  const [socket, setSocket] = useState(null); // Add a socket state
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("message", data => {
+    const newSocket = socketIOClient(ENDPOINT);
+    setSocket(newSocket); // Store the socket instance in the state
+
+    newSocket.on('message', data => {
       setResponse(data);
     });
 
-    return () => socket.disconnect();
+    return () => {
+      newSocket.disconnect();
+    };
   }, []);
 
   const sendMessage = () => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.emit("message", input);
+    socket.emit('message', input); // Use the stored socket instance to send a message
   };
 
   return (
     <div>
-      <input 
-        value={input} 
-        onChange={e => setInput(e.target.value)} 
+      <input
+        value={input}
+        onChange={e => setInput(e.target.value)}
         placeholder="Enter a message"
       />
       <button onClick={sendMessage}>Send</button>
